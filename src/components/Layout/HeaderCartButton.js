a1a1cf2c9bc10de1463197307785a2cb5ac04358
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-01-02 16:30:22 +0100

    Adding items to the cart implementes

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
