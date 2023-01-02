f40e5227f9634c5486ae03a06d09eddaba824822
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-01-02 14:40:19 +0100

    Implemented the open/close of the cart modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,18 +3,20 @@ import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 
 const Cart = (props) => {
-  const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) =>  <li>{item.name}</li>)}</ul>;
+  const cartItems = <ul className={classes['cart-items']}>
+      {[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) =>  <li>{item.name}</li>)}
+    </ul>;
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose} >
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>23.98</span>
       </div>
       <div className={classes.actions}>
-        <buttons className={classes['button--alt']}>Close</buttons>
-        <buttons className={classes.button}>Order</buttons>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
     </Modal>
   )
